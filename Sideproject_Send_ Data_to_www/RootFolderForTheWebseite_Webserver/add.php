b23655c93d989fcb493273c211e3e511e29b2fbb
christianpetri <christianpetri@hotmail.com>
2018-04-09 10:40:23 +0200

    Clean up

--- Sideproject_Send_ Data_to_www/RootFolderForTheWebseite_Webserver/add.php
+++ Sideproject_Send_ Data_to_www/RootFolderForTheWebseite_Webserver/add.php
@@ -3,7 +3,8 @@ include("connect.php");
 date_default_timezone_set("Europe/Paris"); 
 $date = new DateTime();
    
-if (isset($_POST["temp1"])) {
+if (isset($_POST["secret"])) {
+	if($_POST["secret"]) == "1234567890") // Replace with your secret
     $DB->pushUpdate($date->format('Y-m-d H:i:s'),$_POST["temp1"], $_POST["hum1"]);
 }
    
--- Sideproject_Send_ Data_to_www/sendPostToWWW_WiFI_Arduino/sendPostToWWW_WiFI.ino
+++ Sideproject_Send_ Data_to_www/sendPostToWWW_WiFI_Arduino/sendPostToWWW_WiFI.ino
@@ -11,15 +11,16 @@
 #define DHTTYPE DHT22
 #define DHTPIN  2
 
-const char* ssid     = "CP";
-const char* password = "12345678";
-char serverName[] = "www.sensor.christianpetri.ch";
+const char* ssid     = "yourNetwork"; // your network SSID (name)
+const char* password = "PasswordForWifi"; // your network key
+char serverName[] = "www.example.com"; //your websites url // www.sensor.christianpetri.ch
+int secretKey  = 1234567890; //match with your html/php code      
 // change to your server's port
 int serverPort = 80;
 
 // change to the page on that server
 char pageName[] = "/add.php";
-int totalCount = 1; 
+int totalCount = 1;
 // insure params is big enough to hold your variables
 char params[32];
 
@@ -32,34 +33,34 @@ unsigned long lastMillis = 0;
 
 ESP8266WebServer server(80);
 WiFiClient client;
-// Initialize DHT sensor 
+// Initialize DHT sensor
 // NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
 // you need to increase the threshold for cycle counts considered a 1 or 0.
 // You can do this by passing a 3rd parameter for this threshold.  It's a bit
 // of fiddling to find the right value, but in general the faster the CPU the
 // higher the value.  The default for a 16mhz AVR is a value of 6.  For an
 // Arduino Due that runs at 84mhz a value of 30 works.
-// This is for the ESP8266 processor on ESP-01 
+// This is for the ESP8266 processor on ESP-01
 DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
- 
+
 float humidity, temp_c;  // Values read from sensor
 String webString="";     // String to display
 int temperature;
 int lastTemperatureRead = 0;
 int lastHumiditiyRead = 0;
- 
+
 // Generally, you should use "unsigned long" for variables that hold time
 unsigned long previousMillis = 0;        // will store last temp was read
 const long interval = 2000;              // interval at which to read sensor
- 
+
 void handle_root() {
    gettemperature();       // read sensor
-     webString="<html><head><meta http-equiv=\"refresh\" content=\"5\"></head><body>Humidity: "+String((int)humidity)+"% ";
-    webString+="Temperature: "+String((int)temp_c)+"&deg; C </body></html>"; 
+     webString="<html><head><meta http-equiv=\"refresh\" content=\"5\"></head><body>Humidity: " + String((int)humidity)+"% ";
+    webString+="Temperature: " + String((int)temp_c) + "&deg; C </body></html>";
      server.send(200, "text/html", webString);               // send to someones browser when asked
   delay(100);
 }
- 
+
 void setup(void)
 {
   // You can open the Arduino IDE Serial Monitor window to see what the code is doing
@@ -81,9 +82,9 @@ void setup(void)
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-   
+
   server.on("/", handle_root);
-  
+
   server.on("/temp", [](){  // if you add this subdirectory to your webserver call, you get text below :)
     gettemperature();       // read sensor
     webString="<html><head><meta http-equiv=\"refresh\" content=\"5\"></head><body>Temperature:"+String((int)temp_c)+" C</body></html>";   // Arduino has a hard time with float to string
@@ -93,21 +94,21 @@ void setup(void)
   server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
     gettemperature();           // read sensor
     webString="<html><head><meta http-equiv=\"refresh\" content=\"5\"></head><body>Humidity: "+String((int)humidity)+"% ";
-    webString+="Temperature: "+String((int)temp_c)+"&deg; C </body></html>"; 
+    webString+="Temperature: "+String((int)temp_c)+"&deg; C </body></html>";
     server.send(200, "text/html", webString);               // send to someones browser when asked
   });
-  
+
   server.begin();
   Serial.println("HTTP server started");
 }
- 
+
 void loop(void)
 {
   server.handleClient();
   thisMillis = millis();
-  if (WiFi.status() != WL_CONNECTED) { 
+  if (WiFi.status() != WL_CONNECTED) {
       Serial.print("\n\r \n\rWorking to (re-) connect");
-      WiFi.begin(ssid, password); 
+      WiFi.begin(ssid, password);
       int connectLoop = 0;
       while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -115,12 +116,12 @@ void loop(void)
         connectLoop++;
         if(connectLoop > 300)
         {
-         Serial.print("Try again"); 
+         Serial.print("Try again");
          break;
         }
       }
       if(WiFi.status() == WL_CONNECTED) {
-        Serial.println(""); 
+        Serial.println("");
         Serial.print("Connected to ");
         Serial.println(ssid);
         Serial.print("IP address: ");
@@ -133,12 +134,12 @@ void loop(void)
     temperature=dht.readTemperature();
     humidity=dht.readHumidity();
     if(lastTemperatureRead == temperature &&  lastHumiditiyRead == (int)humidity){
-        Serial.println("No Changes"); 
-    } 
+        Serial.println("No Changes");
+    }
     else {
        if(humidity <= 100 && temperature <= 80){
           // params must be url encoded.
-          sprintf(params,"temp1=%i&hum1=%i", temperature, (int)humidity);     
+          sprintf(params,"temp1=%i&hum1=%i&secret=%i", temperature, (int)humidity, secretKey);
           if(!postPage(serverName,serverPort,pageName,params)) Serial.print(F("Fail "));
           else Serial.print(F("Pass "));
           totalCount++;
@@ -146,13 +147,10 @@ void loop(void)
        }
     }
     lastTemperatureRead=temperature;
-    lastHumiditiyRead=(int) humidity;  
-    
-   
+    lastHumiditiyRead=(int) humidity;
   }
- 
-} 
-  
+}
+
 
 void gettemperature() {
   // Wait at least 2 seconds seconds between measurements.
@@ -160,10 +158,10 @@ void gettemperature() {
   // the sensor is bigger than the interval you set, read the sensor
   // Works better than delay for things happening elsewhere also
   unsigned long currentMillis = millis();
- 
+
   if(currentMillis - previousMillis >= interval) {
-    // save the last time you read the sensor 
-    previousMillis = currentMillis;   
+    // save the last time you read the sensor
+    previousMillis = currentMillis;
 
     // Reading temperature for humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
@@ -200,7 +198,7 @@ byte postPage(char* domainBuffer,int thisPort,char* page,char* thisData)
     // send the body (variables)
     client.print(thisData);
     Serial.println(thisData);
-  } 
+  }
   else
   {
     Serial.println(F("failed"));
